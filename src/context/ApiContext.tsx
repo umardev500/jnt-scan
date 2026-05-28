076671f9fcc77b6ec8c97b3c52a592d491194fb2
Umar Schweinsteiger <umar.dev500@gmail.com>
2026-05-28 20:16:49 +0700

    set url 28

--- src/context/ApiContext.tsx
+++ src/context/ApiContext.tsx
@@ -10,7 +10,7 @@ const ApiContext = createContext<ApiContextType | undefined>(undefined);
 const STORAGE_KEY = "api_url";
 
 export const ApiProvider = ({ children }: { children: React.ReactNode }) => {
-  const [apiUrl, setApiUrlState] = useState<string>("https://fireplace-badly-intervention-premiere.trycloudflare.com");
+  const [apiUrl, setApiUrlState] = useState<string>("https://arbitration-weather-logic-seasons.trycloudflare.com");
 
   // ✅ Load from localStorage on startup
   useEffect(() => {
