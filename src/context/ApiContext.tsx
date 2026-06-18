15eba1af8570880ee483d383ae395b30c09796e8
Umar Schweinsteiger <umar.dev500@gmail.com>
2026-06-18 20:25:05 +0700

    set url 18

--- src/context/ApiContext.tsx
+++ src/context/ApiContext.tsx
@@ -10,7 +10,7 @@ const ApiContext = createContext<ApiContextType | undefined>(undefined);
 const STORAGE_KEY = "api_url";
 
 export const ApiProvider = ({ children }: { children: React.ReactNode }) => {
-  const [apiUrl, setApiUrlState] = useState<string>("https://preference-complicated-generate-preservation.trycloudflare.com");
+  const [apiUrl, setApiUrlState] = useState<string>("https://rules-contemporary-treat-homeland.trycloudflare.com");
 
   // ✅ Load from localStorage on startup
   useEffect(() => {
