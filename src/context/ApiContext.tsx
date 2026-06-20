ff9efc11af1b84392b51e1f0ffa4b66315980f95
Umar Schweinsteiger <umar.dev500@gmail.com>
2026-06-20 20:13:32 +0700

    setup 20

--- src/context/ApiContext.tsx
+++ src/context/ApiContext.tsx
@@ -10,7 +10,7 @@ const ApiContext = createContext<ApiContextType | undefined>(undefined);
 const STORAGE_KEY = "api_url";
 
 export const ApiProvider = ({ children }: { children: React.ReactNode }) => {
-  const [apiUrl, setApiUrlState] = useState<string>("https://minimize-aimed-exceed-bless.trycloudflare.com");
+  const [apiUrl, setApiUrlState] = useState<string>("https://wild-learn-till-notes.trycloudflare.com");
 
   // ✅ Load from localStorage on startup
   useEffect(() => {
