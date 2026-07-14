57c887b256e520fca9dc013589aca10de2d17019
Umar Schweinsteiger <umar.dev500@gmail.com>
2026-07-14 20:12:43 +0700

    setup 14

--- src/context/ApiContext.tsx
+++ src/context/ApiContext.tsx
@@ -10,7 +10,7 @@ const ApiContext = createContext<ApiContextType | undefined>(undefined);
 const STORAGE_KEY = "api_url";
 
 export const ApiProvider = ({ children }: { children: React.ReactNode }) => {
-  const [apiUrl, setApiUrlState] = useState<string>("https://each-elegant-aware-scotland.trycloudflare.com");
+  const [apiUrl, setApiUrlState] = useState<string>("https://track-experiment-cabin-deadline.trycloudflare.com");
 
   // ✅ Load from localStorage on startup
   useEffect(() => {
--- src/pages/ReportPage.tsx
+++ src/pages/ReportPage.tsx
@@ -18,8 +18,8 @@ export default function ReportPage() {
 
   const defaultReportFilter: ReportFilter = {
     shipmentState: 3,
-    startTime: formatRaw("2026-07-13T12:00"),
-    endTime: formatRaw("2026-07-14T10:00"),
+    startTime: formatRaw("2026-07-14T12:00"),
+    endTime: formatRaw("2026-07-15T10:00"),
   };
 
   const [vehicleResults, setVehicleResults] = useState<
