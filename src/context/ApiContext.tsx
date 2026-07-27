057ec44d62c82330989232740a17fa8debaadfbd
Umar Schweinsteiger <umar.dev500@gmail.com>
2026-07-27 20:24:16 +0700

    setup 27

--- src/context/ApiContext.tsx
+++ src/context/ApiContext.tsx
@@ -10,7 +10,7 @@ const ApiContext = createContext<ApiContextType | undefined>(undefined);
 const STORAGE_KEY = "api_url";
 
 export const ApiProvider = ({ children }: { children: React.ReactNode }) => {
-  const [apiUrl, setApiUrlState] = useState<string>("https://shade-origins-indiana-metres.trycloudflare.com");
+  const [apiUrl, setApiUrlState] = useState<string>("https://compromise-dod-maintain-schedule.trycloudflare.com");
 
   // ✅ Load from localStorage on startup
   useEffect(() => {
