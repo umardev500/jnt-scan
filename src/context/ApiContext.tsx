fe4037bcba8fc4d29af0f9fb0f876e19e6033770
Umar Schweinsteiger <umar.dev500@gmail.com>
2026-08-06 20:04:28 +0700

    setup 06

--- src/context/ApiContext.tsx
+++ src/context/ApiContext.tsx
@@ -10,7 +10,7 @@ const ApiContext = createContext<ApiContextType | undefined>(undefined);
 const STORAGE_KEY = "api_url";
 
 export const ApiProvider = ({ children }: { children: React.ReactNode }) => {
-  const [apiUrl, setApiUrlState] = useState<string>("https://soa-universe-lamps-reflected.trycloudflare.com");
+  const [apiUrl, setApiUrlState] = useState<string>("https://clinical-beach-dramatic-surveys.trycloudflare.com");
 
   // ✅ Load from localStorage on startup
   useEffect(() => {
